975ec6ce3a3a36d0fbd7aa27cb977f1804010eaf
aaron-wilson <aaron-wilson@users.noreply.github.com>
2025-08-20 14:02:03 -0400

    feat(dev): launch basic GraphQL Yoga server

---
+++ scripts/dev.ts
@@ -0,0 +1,14 @@
+import { createServer } from 'node:http'
+import { createYoga } from 'graphql-yoga'
+import { schema } from '../src/graphql/schema'
+
+// Create a Yoga instance with a GraphQL schema
+const yoga = createYoga({ schema })
+
+// Hook Yoga into an HTTP server
+const server = createServer(yoga)
+
+// Start server
+server.listen(4000, () => {
+  console.info('Server is running on http://localhost:4000/graphql')
+})
